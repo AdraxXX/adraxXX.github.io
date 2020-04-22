d5fbc8ac253ee9e92419eefc915ba4917f80de10
Daniel Johnston <djohn50@uic.edu>
2020-04-22 14:42:44 +0000

    uploading a basic profile.js outline

---
+++ profile.js
@@ -0,0 +1,26 @@
+let project = 
+{
+    "firstName":"Daniel",
+    "address":"1535 fairway st, Naperville IL, 60563",
+    "projects":
+    [
+        {"name":"", "url":"", "status":"", "details":""}
+    ],
+    "experience":
+    [
+        
+    ],
+    "skills":
+    [
+        
+    ],
+    "education":
+    [
+        
+    ],
+    "awards":
+    [
+        
+    ],
+    "about":""
+}
